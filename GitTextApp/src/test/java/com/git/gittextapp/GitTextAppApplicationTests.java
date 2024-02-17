44d084d38683d700fa0790f21d67c5d938d2817b
jenisha <jenishashanrose@gmail.com>
2024-02-17 09:24:20 +0530

    ddd

---
+++ GitTextApp/src/test/java/com/git/gittextapp/GitTextAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.gittextapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTextAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
